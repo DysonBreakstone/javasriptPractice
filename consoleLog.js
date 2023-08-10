830efc3e864f1c8ab5c115214b46a5a8674ce620
DysonBreakstone <121531791+DysonBreakstone@users.noreply.github.com>
2023-08-10 10:23:37 -0600

    first data types

---
+++ consoleLog.js
@@ -0,0 +1,10 @@
+// console.log to print things in terminal
+// node filename.js to run js files in terminal
+
+var gorillaName = "Harambe"
+var gorilla = true
+var gorillaScaleToTen = 10
+
+console.log(gorillaName)
+console.log(gorilla)
+console.log(gorillaScaleToTen)
